1a1816e5714efeba968396b26129ecaad3977571
Lukas Bartak <bartaklukas236@student.voskh.cz>
2025-01-21 09:35:41 +0100

    123

--- script.js
+++ script.js
@@ -5,12 +5,23 @@ const pocetMat = document.getElementById("pocetMat");
 
 // kód vykreslení obrázků
 const pat = "<img src='img/pat.png'>";
+const mat = "<img src='img/mat.png'>";
 
 let kodFigurek = "";
 let i=0;
-while (i<10) {
-  i++;
-  kodFigurek += "<div>" + pat + "</div>";
+let patMat;
+pocetPat = 0
+while (Math.random()*20) {
+  i++; 
+  if (Math.random()<0.5) {
+    patMat = pat;
+    pPat++;
+    }
+  else {
+   patrMat = mat;
+   pMat++;
+  }
+  kodFigurek += "<div>" + patMat + "</div>";
 }
 figurky.innerHTML = kodFigurek; // vloží kód řady obrázků, kterou požadujeme vykreslit
 pocetPat.innerText = i;         // vypíše počet vykreslených figurek Pata (všech deset)
